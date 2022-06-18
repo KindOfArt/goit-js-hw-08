59266e409ecff848240dff047eb6c4754b380fbf
Anton <anton_surkov@rambler.ru>
2022-06-18 18:25:51 +0300

    complete task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryMurkup(galleryItems);
+galleryRef.innerHTML = galleryMarkup;
+
+const gallery = new SimpleLightbox('.gallery  a', {
+  scrollZoom: false,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+galleryRef.addEventListener('click', onGalleryImageClick);
+
+function createGalleryMurkup(gallery) {
+  return gallery
+    .map(image => {
+      return `
+        <a class="gallery__item" href="${image.original}">
+            <img class="gallery__image" src="${image.preview}" alt="${image.description}"/>
+        </a>
+        `;
+    })
+    .join('');
+}
+
+function onGalleryImageClick(e) {
+  e.preventDefault();
+
+  const imageEl = e.target.nodeName;
+
+  if (!imageEl) {
+    return;
+  } else {
+    gallery;
+  }
+}
