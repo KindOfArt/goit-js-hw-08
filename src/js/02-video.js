a0aa299cdcb9bbde304a6b46f8e228c3366f4c1e
Anton <anton_surkov@rambler.ru>
2022-06-20 17:34:25 +0300

    complate task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+// const throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const VIDEOPLAYER_CURRENT_TIME = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const getSavedTime = localStorage.getItem(VIDEOPLAYER_CURRENT_TIME);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+player.setCurrentTime(getSavedTime);
+
+function onTimeUpdate(time) {
+  let savedPausedPlayerTime = time.seconds;
+  localStorage.setItem(VIDEOPLAYER_CURRENT_TIME, savedPausedPlayerTime);
+}
