1b7e2401aaf141f4fe1cd3d3de71001b756491b2
Anton <anton_surkov@rambler.ru>
2022-06-21 15:03:38 +0300

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,50 +6,49 @@ const refs = {
   textarea: document.querySelector('.feedback-form textarea'),
 };
 
-const formData = {};
 const FEEDBACK_FORM_STATE = 'feedback-form-state';
+let feedbackFormData = {
+  email: '',
+  message: '',
+};
 
+refs.form.addEventListener('input', onFormInput);
 refs.form.addEventListener('submit', onFormSubmit);
-refs.email.addEventListener('input', throttle(onInputEmail, 500));
-refs.textarea.addEventListener('input', throttle(onInputTextarea, 500));
 
 populateFormFields();
 
 function onFormSubmit(e) {
   e.preventDefault();
 
-  localStorage.removeItem(FEEDBACK_FORM_STATE);
-
-  const email = e.target.elements.email.value;
-  const message = e.target.elements.message.value;
-  formData.email = email;
-  formData.message = message;
-
-  console.log(formData);
-
-  e.currentTarget.reset();
+  if (
+    feedbackFormData.email.length === 0 ||
+    feedbackFormData.message.length === 0
+  ) {
+    alert('All areas must be filled');
+  }
+  if (
+    feedbackFormData.email.length > 0 &&
+    feedbackFormData.message.length > 0
+  ) {
+    console.log(feedbackFormData);
+    refs.form.reset();
+    localStorage.removeItem(FEEDBACK_FORM_STATE);
+  }
 }
-function onInputEmail(e) {
-  const email = e.target.value;
 
-  formData.email = email;
+function onFormInput(e) {
+  feedbackFormData[e.target.name] = e.target.value;
 
-  setToLocalStorage(FEEDBACK_FORM_STATE, formData);
+  localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(feedbackFormData));
 }
-function onInputTextarea(e) {
-  const message = e.target.value;
 
-  formData.message = message;
-
-  setToLocalStorage(FEEDBACK_FORM_STATE, formData);
-}
-function setToLocalStorage(key, object) {
-  localStorage.setItem(key, JSON.stringify(object));
-}
 function populateFormFields() {
-  const savedMessage = JSON.parse(localStorage.getItem(FEEDBACK_FORM_STATE));
-  if (savedMessage) {
-    refs.email.value = savedMessage.email;
-    refs.textarea.value = savedMessage.message;
+  const savedFeedback = JSON.parse(localStorage.getItem(FEEDBACK_FORM_STATE));
+
+  if (savedFeedback) {
+    feedbackFormData = { ...savedFeedback };
+
+    refs.email.value = feedbackFormData.email;
+    refs.textarea.value = feedbackFormData.message;
   }
 }
